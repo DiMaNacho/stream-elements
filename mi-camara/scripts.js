fcf757d64703c6906130d6babce315432ecf843d
Nacho Martínez <dimanachom@gmail.com>
2021-05-19 18:26:36 -0300

    fix resizing

--- mi-camara/scripts.js
+++ mi-camara/scripts.js
@@ -64,7 +64,7 @@ const fn = () => {
     gotDevices(newDevices);
 
     // Solo para mi
-    videoSelect.selectedIndex = 4;
+    videoSelect.selectedIndex = 5;
     videoSelect.dispatchEvent(new Event("change"));
   };
 
