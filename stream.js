34d590024e1d674c4ae695a41e7b2cbf13dcc4e1
Nacho Martínez <dimanachom@gmail.com>
2021-05-30 21:59:39 -0300

    limpieza de todo

--- stream.js
+++ stream.js
@@ -1,21 +1,15 @@
 const stream = {
   invitado: {
-    nombre: "@UruguayTwitch",
+    nombre: "@yosoyandyok",
     red: "ig",
-    camara:
-      "http://obs.ninja/?bitrate=3000&framerate=60&width=1280&height=768&stereo=1&codec=av1&optimize&password=74123&scen&room=nmlc2021&view=WeQMunb",
   },
   yo: {
     nombre: "@DiMaNacho",
     red: "ig",
-    camara:
-      "http://obs.ninja/?bitrate=3000&framerate=60&width=1280&height=768&stereo=1&codec=av1&optimize&password=74123&scene&room=nmlc2021&view=WeQMunb",
   },
   columna: {
     columna: "Columna MundoUno",
-    nombre: "@NachoVerdeOficial",
+    nombre: "@leandro.sli",
     red: "ig",
-    camara:
-      "http://obs.ninja/?bitrate=3000&framerate=60&width=1280&height=768&stereo=1&codec=av1&optimize&password=74123&scene&room=nmlc2021&view=WeQMunb",
   },
 };
